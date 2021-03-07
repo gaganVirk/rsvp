e15e7a07d9a900f0eccf8bfe67994f104c8e4212
gagan <deep.virk@outlook.co.nz>
2021-03-08 12:09:57 +1300

    filtered guests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import GuestList from './GuestList';
 class App extends Component {
 
   state = {
+    isFiltered: false,
     guests: [
       {
         name: 'Treasure',
@@ -36,6 +37,9 @@ class App extends Component {
         return guest;
       })
     });
+
+    toggleFilter = () => 
+      this.setState({ isFiltered: !this.state.isFiltered });
 
     toggleConfirmationAt = index =>
       this.toggleGuestPropertyAt("isConfirmed", index);
@@ -75,7 +79,10 @@ class App extends Component {
           <div>
             <h2>Invitees</h2>
             <label>
-              <input type="checkbox" /> Hide those who haven't responded
+              <input 
+                type="checkbox" 
+                onChange={this.toggleFilter}
+                checked={this.state.isFiltered} /> Hide those who haven't responded
             </label>
           </div>
           <table className="counter">
@@ -99,7 +106,9 @@ class App extends Component {
             guests={this.state.guests}
             toggleConfirmationAt={this.toggleConfirmationAt}
             toggleEditingAt={this.toggleEditingAt} 
-            setNameAt={this.setNameAt}/>
+            setNameAt={this.setNameAt}
+            isFiltered={this.state.isFiltered}
+            />
         </div>
       </div>
     );
--- src/GuestList.js
+++ src/GuestList.js
@@ -4,15 +4,17 @@ import Guest from './Guest';
 
 const GuestList = props =>
     <ul>
-        {props.guests.map((guest, index) =>
-            <Guest 
-                key={index} 
-                name={guest.name} 
-                isConfirmed={guest.isConfirmed} 
-                isEditing={guest.isEditing}
-                handleConfirmation={() => props.toggleConfirmationAt(index)} 
-                handleToggleEditing={() => props.toggleEditingAt(index)}
-                setName={text => props.setNameAt(text, index)} />
+        {props.guests
+            .filter(guest => !props.isFiltered || guest.isConfirmed)
+            .map((guest, index) =>
+                <Guest 
+                    key={index} 
+                    name={guest.name} 
+                    isConfirmed={guest.isConfirmed} 
+                    isEditing={guest.isEditing}
+                    handleConfirmation={() => props.toggleConfirmationAt(index)} 
+                    handleToggleEditing={() => props.toggleEditingAt(index)}
+                    setName={text => props.setNameAt(text, index)} />
         )}
     </ul>;
 
@@ -20,7 +22,8 @@ GuestList.propTypes = {
     guests: PropTypes.array.isRequired,
     toggleConfirmationAt: PropTypes.func.isRequired,
     toggleEditingAt: PropTypes.func.isRequired,
-    setNameAt: PropTypes.func.isRequired
+    setNameAt: PropTypes.func.isRequired,
+    isFiltered: PropTypes.bool.isRequired
 };
 
 export default GuestList;
